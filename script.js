475e228d4daffd812985d268370a6e91ee288434
QiLin Xue <645367@pdsb.net>
2019-02-19 18:44:50 -0500

    add random quote generator and revealer for quiz tomorrow

--- script.js
+++ script.js
@@ -6,8 +6,35 @@ function getQuote(){
     document.getElementById("quotation").innerHTML = x;
 }
 
+var act, scene, line = 0;
+var lineId = act+"."+scene+"."+line
+function randomQuote(){
+    while(true){
+        act = Math.floor(Math.random() * 2) + 1
+        scene = Math.floor(Math.random() * 7) + 1
+        line =  Math.floor(Math.random() * 7) + 1
+        lineId = act+"."+scene+"."+line
+        try{
+            document.getElementById("quotation").innerHTML = document.getElementsByName(lineId)[0].innerHTML
+            break;
+        }
+        catch{
+            continue;
+        }
+    }
+}
+
 function scrollQuote(){
-    document.getElementsByName(input.value)[0].scrollIntoView({block: "start", behavior: "smooth"});
+    try{
+        document.getElementsByName(input.value)[0].scrollIntoView({block: "start", behavior: "smooth"});
+    }
+    catch{
+        document.getElementsByName(lineId)[0].scrollIntoView({block: "start", behavior: "smooth"});
+    }
+}
+
+function revealRandom(){
+    document.getElementById("quotation").innerHTML = document.getElementsByName(lineId)[0].innerHTML + "<br/>Source: " + lineId
 }
 
 input.addEventListener("keyup", function(event) {
